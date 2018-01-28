686813947434acbb2967af7e1aac8fc9ab3c6d9b
shushu <shushu0212@gami.com>
2018-01-28 11:17:20 +0800

    add form on index page for creating new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,25 @@ class Admin::CategoriesController < ApplicationController
   before_action :authenticate_admin
 
   def index
+    @category = Category.new
     @categories = Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "category was successfully created"
+      redirect_to admin_categories_path
+    
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
