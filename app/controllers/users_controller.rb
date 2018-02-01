e8dea5d38a2da2a239e08e1cfcc64ad94eb2f431
shushu <shushu0212@gami.com>
2018-02-02 00:21:01 +0800

    add user edit and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "profile was successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "profile was failed to update"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
 end
