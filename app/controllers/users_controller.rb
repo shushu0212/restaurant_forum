77895357d8a2fc616cf8a38784aef0016a57d571
shushu <shushu0212@gami.com>
2018-02-03 11:44:10 +0800

    profile edit add user authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
+    unless @user == current_user
+      flash[:alert] = "非本人，無法編輯!"
+      redirect_to root_path
+    end
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "profile was successfully updated"
       redirect_to user_path(@user)
@@ -18,7 +21,11 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
+  def set_user
+      @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :intro, :avatar)
