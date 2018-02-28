d16a63a5561d1781a5fd07298e55660f466a40c1
shushu <shushu0212@gami.com>
2018-02-28 10:23:45 +0800

    validate uniqueness of following_id in user_id scope/add create action for followship record

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,7 @@
 class Followship < ApplicationRecord
   belongs_to :user
-  belongs_to :folowing, class_name: "User" 
+  belongs_to :following, class_name: "User" 
+
+  # 「每個 User 只能追蹤另一個 User 一次」，從資料表的角度來描述，就是「特定的 user_id 下，只能有一個 followings_id」
+  validates :following_id, uniqueness: { scope: :user_id }
 end
